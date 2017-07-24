b09e4c6639f3446a8ee4ba9991fc7b2a1b5bab6f
SurajRepo <verma.surajcool@gmail.com>
2017-07-24 15:24:59 +0545

    DTO added

---
+++ VPortal.Api/DTO/Department.cs
@@ -0,0 +1,12 @@
+namespace VPortal.Api.DTO
+{
+    [Table("Departments")]
+    public class Department 
+    {
+        [Key]
+        public int DepartmentId { get; set; }
+        public string DepartmentCode { get; set; }
+        public string DepartmentName { get; set; }
+        public bool Deleted { get; set; }
+    }
+}
