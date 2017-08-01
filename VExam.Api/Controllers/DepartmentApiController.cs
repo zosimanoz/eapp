76f49ad5d9392a49750949c8a119cad72b4c555b
Manoj Joshi <contact.me.manoz@gmail.com>
2017-08-01 18:42:21 +0545

    Department api updated and added

--- VExam.Api/Controllers/DepartmentApiController.cs
+++ VExam.Api/Controllers/DepartmentApiController.cs
@@ -29,11 +29,13 @@ namespace VExam.Api.Controllers
         {
             try
             {
-                Console.WriteLine("hlw ma yeha xu");
-                Console.WriteLine("code: ");
-                var result = await _departmentService.CrudService.InsertAsync(model);
-                     Console.WriteLine(result.Value);
-                return HttpResponse(200, "", result.Value);
+                if(model.DepartmentId == 0){
+                    var result = await _departmentService.CrudService.InsertAsync(model);
+                    return HttpResponse(200, "", result.Value);
+                }else {
+                    var result = await _departmentService.CrudService.UpdateAsync(model);
+                    return HttpResponse(200, "", result);
+                }
             }
             catch (Exception e)
             {
@@ -41,6 +43,21 @@ namespace VExam.Api.Controllers
                 return HttpResponse(500, model.DepartmentCode);
             }
         }
+
+
+        [HttpGet]
+        [Route("get/{id}")]
+        public async Task<ApiResponse> GetById(int id){
+            try{
+                var result = await _departmentService.CrudService.GetAsync(id);
+                return HttpResponse(200, "", result);
+            }catch(Exception e){
+                _logger.Log(LogType.Error, () => e.Message, e);
+                return HttpResponse(500, e.Message);
+            }
+        }
+
+
         [HttpGet]
         //  [Authorize]
         [Route("get/all")]
